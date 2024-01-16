5944203582dc52089dba862248b9bccac82a7e14
Thomas Janke <janke@tu-berlin.de>
2024-01-16 01:33:46 +0100

    Ignore PostConstruct exception in PodWatcher and ServiceWatcher

--- src/main/kotlin/io/hawk/service/module/cluster/PodWatcher.kt
+++ src/main/kotlin/io/hawk/service/module/cluster/PodWatcher.kt
@@ -5,6 +5,7 @@ import io.fabric8.kubernetes.client.KubernetesClient
 import io.fabric8.kubernetes.client.Watcher
 import io.hawk.service.module.ReconnectingWatcher
 import jakarta.annotation.PostConstruct
+import org.apache.logging.log4j.LogManager
 import org.springframework.boot.autoconfigure.condition.ConditionalOnCloudPlatform
 import org.springframework.boot.cloud.CloudPlatform
 import org.springframework.stereotype.Component
@@ -15,9 +16,15 @@ class PodWatcher(
     private val kubernetesClient: KubernetesClient,
     private val clusterCollector: ClusterCollector
 ) : ReconnectingWatcher<Pod>() {
+    private val logger = LogManager.getLogger(javaClass)
+
     @PostConstruct
     override fun initialize() {
-        kubernetesClient.pods().watch(this)
+        try {
+            kubernetesClient.pods().watch(this)
+        } catch (e: Exception) {
+            logger.error("Watching kubernetes pods failed", e)
+        }
     }
 
     override fun eventReceived(action: Watcher.Action, pod: Pod) {
--- src/main/kotlin/io/hawk/service/module/cluster/ServiceWatcher.kt
+++ src/main/kotlin/io/hawk/service/module/cluster/ServiceWatcher.kt
@@ -5,6 +5,7 @@ import io.fabric8.kubernetes.client.KubernetesClient
 import io.fabric8.kubernetes.client.Watcher
 import io.hawk.service.module.ReconnectingWatcher
 import jakarta.annotation.PostConstruct
+import org.apache.logging.log4j.LogManager
 import org.springframework.boot.autoconfigure.condition.ConditionalOnCloudPlatform
 import org.springframework.boot.cloud.CloudPlatform
 import org.springframework.stereotype.Component
@@ -15,9 +16,15 @@ class ServiceWatcher(
     private val kubernetesClient: KubernetesClient,
     private val clusterCollector: ClusterCollector
 ) : ReconnectingWatcher<Service>() {
+    private val logger = LogManager.getLogger(javaClass)
+
     @PostConstruct
     override fun initialize() {
-        kubernetesClient.services().watch(this)
+        try {
+            kubernetesClient.services().watch(this)
+        } catch (e: Exception) {
+            logger.error("Watching kubernetes services failed", e)
+        }
     }
 
     override fun eventReceived(action: Watcher.Action, service: Service) {
